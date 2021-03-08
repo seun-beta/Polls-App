aff3fd3f2474c707f2430025685512da170cc6a6
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-08 21:19:35 +0100

    created model for polls. Question and Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(
+        max_length=265,
+        help_text= 'Input your question',
+        validators=[MinLengthValidator(1, 'Input 1 character and above')]
+    )
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=256)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+        
