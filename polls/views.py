e455217dc189b51e92f945e497ed71ae6fc12c7e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-08 21:20:37 +0100

    create view for handling 'home' requests

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from polls.models import Question, Choice
+
+class HomeView(View):
+    def get(self, request):
+        poll = Question.objects.all()
+        ctx = { 'poll_list' : poll }
+        return render(request, 'polls/home.html', ctx)
+
+
+class NewPoll(CreateView):
+    model = Question
+    success_url = reverse_lazy('polls:home')
+    fields = '__all__'
